5951a428434bcbb59e5045f9b1d98ca91e859294
Лисин Константин Сергеевич <gr604_likse@ttit.local>
2023-05-19 09:55:55 +0700

    Add Sub fun Err

--- Subtraction.cs
+++ Subtraction.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Calculator
+﻿namespace Calculator
 {
     internal class Subtraction
     {
-    Csd
+        public static double Subtract(double x, double y)
+        {
+            return x - y;
+        }
     }
 }
